c9c2fe4d75b8af2c0a4d4ea3dd3a629e25a1c6f5
shivakant786 <shivakanttiwari123@gmail.com>
2024-10-11 15:20:20 +0530

    add creditial

--- configs/CloudanaryConfig.js
+++ configs/CloudanaryConfig.js
@@ -2,9 +2,9 @@ const cloudinary = require('cloudinary').v2;
 
 // Configure Cloudinary with your credentials
 cloudinary.config({
-  cloud_name: 'djoeqifdy',
-  api_key: '928877257269544',
-  api_secret: 'e5Ph7DXI-BcPy3nBR-wDa6-7sCc',
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET_KEY,
 });
 
 module.exports = cloudinary;
